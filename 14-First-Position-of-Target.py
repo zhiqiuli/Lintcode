4618440191b0e4a7487c0a110e351f01199d8713
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-11-24 22:55:45 -0500

    Create 14-First-Position-of-Target.py

---
+++ 14-First-Position-of-Target.py
@@ -0,0 +1,34 @@
+# https://www.lintcode.com/problem/14/description
+
+class Solution:
+    """
+    @param nums: The integer array.
+    @param target: Target to find.
+    @return: The first position of target. Position starts from 0.
+    """
+    def binarySearch(self, nums, target):
+        # write your code here
+        if not nums:
+            return -1
+        
+        start, end = 0, len(nums)-1
+        
+        # 1
+        while start + 1 < end:
+            
+            # 2
+            mid = start + int((end-start)/2)
+
+            # 3 如果nums[mid] >= target,则题解一定在左边，否则题解在右边
+            if nums[mid] >= target:
+                end = mid
+            else:
+                start = mid
+        
+        # 4
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        
+        return -1
