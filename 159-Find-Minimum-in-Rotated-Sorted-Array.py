1a983b92198e9a7cd64983a733f0cde921fcf800
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-11-26 23:14:36 -0500

    Create 159-Find-Minimum-in-Rotated-Sorted-Array.py

---
+++ 159-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -0,0 +1,33 @@
+# https://www.lintcode.com/problem/159/description
+
+class Solution:
+    """
+    @param nums: a rotated sorted array
+    @return: the minimum number in the array
+    """
+    def findMin(self, nums):
+        # write your code here
+        if not nums:
+            return -1
+
+        start, end = 0, len(nums) - 1
+
+        # the first position where <= the last number
+        target = nums[-1]
+
+        # 1
+        while start + 1 < end:
+
+            # 2
+            mid = start + int((end - start)/2)
+
+            # 3 the first position where <= the last number
+            if nums[mid] <= target:
+                end = mid
+            else:
+                start = mid
+
+        if nums[start] > nums[end]:
+            return nums[end]
+        else:
+            return nums[start]
