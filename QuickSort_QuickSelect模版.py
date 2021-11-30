ff50f6415b7695111cb8f30b8f006b6b67d51b8c
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-11-30 15:52:36 -0500

    Create QuickSort_QuickSelect模版.py

---
+++ QuickSort_QuickSelect模版.py
@@ -0,0 +1,55 @@
+'''
+九章quick select 模板 -  LeetCode 215. Kth Largest Element in an Array
+'''
+class Solution:
+ def findKthLargest(self, nums: List[int], k: int) -> int:
+     # kth larget = n - k th smallest
+     return self.partition(nums, 0, len(nums) - 1, len(nums) - k)
+
+ def partition(self, nums, start, end, k):
+     # find kth smallest element in nums, here k is representing the index, and it starts from 0
+     # only consider the index [start : end]
+     # the following if ... return is not necessary
+     if start == end:
+        return nums[k]
+     l, r = start, end
+     p = nums[(l + r) // 2]
+     while l <= r:
+         while l <= r and nums[l] < p:
+             l += 1
+         while l <= r and nums[r] > p:
+             r -= 1
+         if l <= r:
+             nums[l], nums[r] = nums[r], nums[l]
+             l += 1; r -= 1
+     # right <= left by stoping condition
+     if k <= r:
+         return self.partition(nums, start, r, k)
+     if k >= l:
+         return self.partition(nums, l, end, k)
+     return nums[k]
+
+'''
+quick sort 模板 - LeetCode 912. Sort an Array
+'''
+
+class Solution:
+ def sortArray(self, nums: List[int]) -> List[int]:
+     self.quick_sort(nums, 0, len(nums) - 1)
+     return nums 
+
+ def quick_sort(self, nums, start, end):
+     if start >= end: return
+     l, r = start, end
+     m = (l + r) // 2
+     target = nums[m]
+     while l <= r:
+         while l <= r and nums[l] < target:
+             l += 1
+         while l <= r and nums[r] > target:
+             r -= 1
+         if l <= r:
+             nums[l], nums[r] = nums[r], nums[l]
+             l += 1; r -= 1
+     self.quick_sort(nums, start, r)
+     self.quick_sort(nums, l, end)
