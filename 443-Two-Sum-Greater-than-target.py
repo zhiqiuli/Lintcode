2b1c572af9c4664016d8c6ac0f2f548e2759c9e7
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-11-30 08:32:28 -0500

    Create 443-Two-Sum-Greater-than-target.py

---
+++ 443-Two-Sum-Greater-than-target.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param nums: an array of integer
+    @param target: An integer
+    @return: an integer
+    """
+    def twoSum2(self, nums, target):
+        # write your code here
+        if not nums:
+            return 0
+        nums.sort()
+        count = 0
+        left, right = 0, len(nums) - 1
+        while left < right:
+            if nums[left] + nums[right] > target:
+                count += (right - left)
+                right -= 1
+            else:
+                left += 1
+        
+        return count
