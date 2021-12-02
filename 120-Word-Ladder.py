de34075081fa52fb28ad92d0f8c9690ec98beccf
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-02 15:09:37 -0500

    Create 120-Word-Ladder.py

---
+++ 120-Word-Ladder.py
@@ -0,0 +1,40 @@
+# https://www.lintcode.com/problem/120/description?_from=collection&fromId=161
+
+class Solution:
+    """
+    @param: start: a string
+    @param: end: a string
+    @param: dict: a set of string
+    @return: An integer
+    """
+    def ladderLength(self, start, end, dict):
+        dict.add(end) # make sure the last element is saved in the result dict
+        queue = collections.deque([start])
+        visited = set([start])
+        distance = 0
+
+        while queue:
+            distance += 1            
+            for _ in range(len(queue)): # 分层的bfs，需要range(len(queue))
+                word = queue.popleft()
+                if word == end:
+                    return distance
+                
+                # total is O(26*L^2)
+                for next_word in self.find_next_words(word, dict):
+                    if next_word not in dict or next_word in visited: # O(L)
+                        continue
+                    queue.append(next_word)
+                    visited.add(next_word)
+
+        return 0
+    
+    def find_next_words(self, word, dictionary):
+        words = []
+        for i in range(len(word)): # O(L)
+            left, right = word[:i], word[i+1:]
+            for k in 'qwertyuiopasdfghjklzxcvbnm': # O(26)
+                if word[i] == k: # make sure the same one is not included
+                    continue
+                words.append(left + k + right)
+        return words
