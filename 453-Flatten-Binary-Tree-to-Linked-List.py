37d99b7c2ba937a32a4d656a2674615dda636260
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-06 21:06:55 -0500

    Create 453-Flatten-Binary-Tree-to-Linked-List.py

---
+++ 453-Flatten-Binary-Tree-to-Linked-List.py
@@ -0,0 +1,34 @@
+# https://www.lintcode.com/problem/453/description?_from=collection&fromId=161
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        return self.helper(root)
+
+    def helper(self, root):
+        if root is None:
+            return None
+
+        # 假设左边右边已经排序好了
+        left_last  = self.helper(root.left)
+        right_last = self.helper(root.right)
+
+        # 将left_last的右边的node接到root.right
+        if left_last is not None:
+            left_last.right = root.right
+            root.right = root.left
+            root.left = None
+        
+        # return的顺序很重要 right, left and root
+        return right_last or left_last or root
