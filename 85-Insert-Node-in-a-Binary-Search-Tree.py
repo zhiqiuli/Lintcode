fcc50125231813d66dd15bc2a85423dc79679db9
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-07 09:08:53 -0500

    Update 85-Insert-Node-in-a-Binary-Search-Tree.py

--- 85-Insert-Node-in-a-Binary-Search-Tree.py
+++ 85-Insert-Node-in-a-Binary-Search-Tree.py
@@ -6,7 +6,9 @@ class TreeNode:
         self.left, self.right = None, None
 """
 
-
+###
+### recursive
+###
 # https://www.lintcode.com/problem/85/description?_from=collection&fromId=161
 class Solution:
     """
@@ -24,4 +26,32 @@ class Solution:
             root.right = self.helper(root.right, node)
         if node.val < root.val:
             root.left  = self.helper(root.left , node)
+        return root
+
+
+###
+### non-recursive
+###
+# https://www.lintcode.com/problem/85/description?_from=collection&fromId=161
+class Solution:
+    """
+    @param: root: The root of the binary search tree.
+    @param: node: insert this node into the binary search tree
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        if root is None:
+            return node
+        
+        curr = root
+        while curr != node:
+            if node.val > curr.val:
+                if curr.right is None:
+                    curr.right = node
+                curr = curr.right
+            else:
+                if curr.left is None:
+                    curr.left = node
+                curr = curr.left
+
         return root
