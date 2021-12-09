97a6348fa6cfa2bafe53b359aefdbcc2ac521fba
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-09 09:50:59 -0500

    Create 15-Permutations.py

---
+++ 15-Permutations.py
@@ -0,0 +1,30 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: A list of permutations.
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        visited = [False] * len(nums)
+        self.dfs(nums, [], res, visited)
+        return res
+    
+    def dfs(self, nums, perm, res, visited):
+        # 递归出口
+        if len(nums) == len(perm):
+            res.append(perm[:])
+        
+        for i in range(len(nums)):
+            # 跳过已经访问过的元素
+            if visited[i]:
+                continue
+            # 经典dfs模版
+            perm.append(nums[i])
+            visited[i] = True
+            self.dfs(nums, perm, res, visited)
+            perm.pop()
+            visited[i] = False
