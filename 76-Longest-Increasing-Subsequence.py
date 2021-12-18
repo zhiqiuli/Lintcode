2a66baa5864c2c1434260bf8a31f622e3b0575eb
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-18 17:00:15 -0500

    Create 76-Longest-Increasing-Subsequence.py

---
+++ 76-Longest-Increasing-Subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        if not nums:
+            return 0
+        dp = [1] * len(nums) # 结尾是dp[i]的LIS是多少
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
