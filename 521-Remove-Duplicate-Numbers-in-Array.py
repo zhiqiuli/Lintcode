1ee43a981d68f8079e007dc9bb2c2b41729e7e53
zhiqiuli <zhiqiuli.edu@gmail.com>
2021-12-19 15:12:16 -0500

    Update 521-Remove-Duplicate-Numbers-in-Array.py

--- 521-Remove-Duplicate-Numbers-in-Array.py
+++ 521-Remove-Duplicate-Numbers-in-Array.py
@@ -15,3 +15,21 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
                 j -= 1
         return i
+
+    
+class Solution:
+    """
+    @param nums: an array of integers
+    @return: the number of unique integers
+    """
+    def deduplication(self, nums):
+        if not nums: return 0
+        if len(nums) == 1: return 1
+        nums.sort() # 这个题只能用.sort()来做
+        k = 0
+        for i in range(1, len(nums)):
+            print(i, k, nums)
+            if nums[k] != nums[i]:
+                k += 1
+                nums[k], nums[i] = nums[i], nums[k]
+        return k + 1
