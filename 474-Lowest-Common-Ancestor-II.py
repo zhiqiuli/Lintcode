00e6c9c961a631e34b07921e89ba813348722b58
zhiqiuli <zhiqiuli.edu@gmail.com>
2022-01-04 10:44:56 -0500

    Create 474-Lowest-Common-Ancestor-II.py

---
+++ 474-Lowest-Common-Ancestor-II.py
@@ -0,0 +1,26 @@
+"""
+Definition of ParentTreeNode:
+class ParentTreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.parent, self.left, self.right = None, None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the tree
+    @param: A: node in the tree
+    @param: B: node in the tree
+    @return: The lowest common ancestor of A and B
+    """
+    def lowestCommonAncestorII(self, root, A, B):
+        A_path_set = set()
+        while A:
+            A_path_set.add(A)
+            A = A.parent
+        while B:
+            if B in A_path_set:
+                return B
+            B = B.parent
+        return None
