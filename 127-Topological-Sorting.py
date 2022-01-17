6c797815caf93b60ff04f513157253ef09e6a20c
zhiqiuli <zhiqiuli.edu@gmail.com>
2022-01-16 22:36:22 -0500

    Update 127-Topological-Sorting.py

--- 127-Topological-Sorting.py
+++ 127-Topological-Sorting.py
@@ -16,15 +16,19 @@ class Solution:
         order = []
         node_to_indegree = self.get_indegree(graph)
         queue = collections.deque([x for x in node_to_indegree if node_to_indegree[x] == 0])
+        # level = 0 (*)
         while queue:
+            # for _ in range(len(queue) (*)
             q = queue.popleft()
             order.append(q)
             for n in q.neighbors:
                 node_to_indegree[n] -= 1 # order.append()以后就需要indegree-1
                 if node_to_indegree[n] == 0: # 如何indgegree成为0，自然放进queue当中
                     queue.append(n)
+            # level += 1 (*)
         
         return order
+    # (*) means if we want to make sure that ***how many levels we need in order to cover all the nodes***.
     
     # find nodes with indegree == 0
     def get_indegree(self, graph):
