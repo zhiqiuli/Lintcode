7dce8edc18c321bf7f0ab71ee87b09395e135d55
zhiqiuli <zhiqiuli.edu@gmail.com>
2022-01-19 22:09:04 -0500

    Create 125-Backpack-II.py

---
+++ 125-Backpack-II.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param m: An integer m denotes the size of a backpack
+    @param A: Given n items with size A[i]
+    @param V: Given n items with value V[i]
+    @return: The maximum value
+    """
+    def backPackII(self, m, A, V):
+        if sum(A) <= m:
+            return sum(V)
+        n = len(A)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if j - A[i - 1] < 0:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], \
+                                   dp[i - 1][j - A[i - 1]] + V[i - 1])
+        return dp[n][m]
