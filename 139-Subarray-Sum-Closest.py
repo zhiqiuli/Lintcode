ddc6c9dc4caa77118d2bcfa6d5e2a9b0e2503672
zhiqiuli <zhiqiuli.edu@gmail.com>
2022-01-25 22:09:02 -0500

    Update 139-Subarray-Sum-Closest.py

--- 139-Subarray-Sum-Closest.py
+++ 139-Subarray-Sum-Closest.py
@@ -9,7 +9,8 @@ class Solution:
     """
     def subarraySumClosest(self, nums):
         prefix_sum = 0
-        prefix_sum_list = [(0, -1)] # initialize
+        # initialization as [(0, -1)] is THE KEY
+        prefix_sum_list = [(0, -1)]
         for i in range(len(nums)): # O(N)
             prefix_sum += nums[i]
             # stores prefix_sum and index of position
@@ -22,7 +23,8 @@ class Solution:
         for i in range(1, len(prefix_sum_list)): # O(N)
             if prefix_sum_list[i][0] - prefix_sum_list[i - 1][0] < diff:
                 diff = prefix_sum_list[i][0] - prefix_sum_list[i - 1][0]
-                # +1和initialize数据有关系，考虑当只有一个元素100时，[(0,-1), (177,0)] -> left = min(-1,0)+1 = 0 & right = max(-1,0) = 0
+                # left_index+1 is THE KEY
+                # +1和initialize数据有关系，考虑当只有一个元素nums=[100]时，prefix_sum_list=[(0,-1), (100,0)] -> left = min(-1,0)+1 = 0 & right = max(-1,0) = 0
                 left_index  = min(prefix_sum_list[i][1], prefix_sum_list[i - 1][1]) + 1
                 right_index = max(prefix_sum_list[i][1], prefix_sum_list[i - 1][1])
         return [left_index, right_index]
