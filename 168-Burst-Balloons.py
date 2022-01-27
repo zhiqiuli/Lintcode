cccf6196566c52060083a49fda9cd12509116d7f
zhiqiuli <zhiqiuli.edu@gmail.com>
2022-01-27 09:36:09 -0500

    Update 168-Burst-Balloons.py

--- 168-Burst-Balloons.py
+++ 168-Burst-Balloons.py
@@ -24,3 +24,31 @@ class Solution:
         
         memo[(i, j)] = curr_max
         return curr_max
+
+    
+    
+'''
+上传侯卫东老师在算法强化班中的解法，区间型，区间长度from 3 to n。
+初始化区间长度==2时，dp为0. 两个数相邻，没有中间的value，所以初始化成0.
+'''
+class Solution:
+    """
+    @param nums: A list of integer
+    @return: An integer, maximum coins
+    """
+    def maxCoins(self, nums):
+        if not nums:
+            return 0
+            
+        nums = [1, *nums, 1]
+        n = len(nums)
+
+        dp = [[0] * n for _ in range(n)]
+
+        for length in range(3, n + 1): # length from 3 to n, inclusively
+            for i in range(n + 1 - length):
+                j = i + length - 1
+                for k in range(i + 1, j): # k is between i and j, exclusively
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[j] * nums[k])
+        
+        return dp[0][-1]
