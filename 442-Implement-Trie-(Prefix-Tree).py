dff829aa2dcc147e4dcae653d9d9c06feb03ba05
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-01-29 22:02:09 -0500

    Update 442-Implement-Trie-(Prefix-Tree).py

--- 442-Implement-Trie-(Prefix-Tree).py
+++ 442-Implement-Trie-(Prefix-Tree).py
@@ -1,3 +1,6 @@
+###
+### Method 1
+###
 class TrieNode:
 
     def __init__(self):
@@ -49,3 +52,60 @@ class Trie:
     def startsWith(self, prefix):
         # write your code here
         return self.find(prefix) is not None
+
+    
+    
+    
+###
+### Method 2
+###
+class TrieNode:
+    
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    
+    def __init__(self):
+        # do intialization if necessary
+        self.root = TrieNode()
+
+    """
+    @param: word: a word
+    @return: nothing
+    """
+    def insert(self, word):
+        # write your code here
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+    
+    """
+    @param: word: A string
+    @return: if the word is in the trie.
+    """
+    def search(self, word):
+        # write your code here
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_word
+
+    """
+    @param: prefix: A string
+    @return: if there is any word in the trie that starts with the given prefix.
+    """
+    def startsWith(self, prefix):
+        # write your code here
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
