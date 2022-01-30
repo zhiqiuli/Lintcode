7f4bbcceebea27391a029459773f8d0a42c8c9db
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-01-29 23:02:42 -0500

    Create 473-Add-and-Search-Word-Data-structure-design.py

---
+++ 473-Add-and-Search-Word-Data-structure-design.py
@@ -0,0 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    """
+    @param: word: Adds a word into the data structure.
+    @return: nothing
+    """
+    def addWord(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+
+    """
+    @param: word: A word could contain the dot character '.' to represent any one letter.
+    @return: if the word is in the data structure.
+    """
+    def search(self, word):
+        return self.dfs(word, self.root, 0)
+    
+    def dfs(self, word, node, index):
+        if node is None:
+            return False
+
+        if len(word) == index:
+            return node.is_word
+        
+        char = word[index]
+        if char != '.':
+            return self.dfs(word, node.children.get(char), index + 1)
+        
+        for child in node.children:
+            if self.dfs(word, node.children[child], index + 1):
+                return True
+        
+        return False
