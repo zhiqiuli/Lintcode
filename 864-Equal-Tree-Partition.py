391c2bde98af9ee2492b45983425c39b7407ef33
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-03-08 15:02:20 -0500

    Update 864-Equal-Tree-Partition.py

--- 864-Equal-Tree-Partition.py
+++ 864-Equal-Tree-Partition.py
@@ -33,3 +33,48 @@ class Solution:
         if root is None:
             return 0
         return root.val + self.pathSum(root.left) + self.pathSum(root.right)
+
+    
+    
+    
+    
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode
+    @return: return a boolean
+    """
+    def checkEqualTree(self, root):
+        self.memo = {}
+        total_sum = self.traverse(root)
+        queue = collections.deque([root])
+        while queue:
+            node = queue.popleft()
+            curr_sum = self.traverse(node)
+            if node != root and total_sum - curr_sum == curr_sum:
+                return True
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return False
+
+    def traverse(self, root):
+        if root is None:
+            return 0
+
+        if root in self.memo:
+            return self.memo[root]
+
+        self.memo[root] = root.val
+        self.memo[root] += self.traverse(root.left)
+        self.memo[root] += self.traverse(root.right)
+        
+        return self.memo[root]
