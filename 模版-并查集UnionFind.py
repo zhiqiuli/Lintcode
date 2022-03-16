b4288db320016040bee73fc1ef2f8d2f72acf89a
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-03-15 23:15:33 -0400

    Update 并查集

--- 模版-并查集UnionFind.py
+++ 模版-并查集UnionFind.py
@@ -3,7 +3,7 @@ Disjoint-set/Union-find Forest
 Find(x): find the root/cluster-id of x
 Union(x, y): merge two clusters
  
-Check whether two elements are in the same set or not in O(1).
+Check whether two elements are in the same set or not in O(1)*.
  
 Find: O(a(n))* ~= O(1)
 Union: O(a(n))* ~= O(1)
