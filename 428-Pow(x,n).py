bc0f5a7a438a8e26141484b4cd9e0c293ccf05ac
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-03-24 22:32:54 -0400

    Update 428-Pow(x,n).py

--- 428-Pow(x,n).py
+++ 428-Pow(x,n).py
@@ -1,3 +1,30 @@
+# 二刷
+class Solution:
+    """
+    @param x {float}: the base number
+    @param n {int}: the power number
+    @return {float}: the result
+    """
+    def myPow(self, x, n):
+        if n == 0: return 1
+        if n == 1: return x
+        if n < 0:
+            x = 1/x
+            n = -n
+        res = 1
+        while n > 0:
+            # 此时不累计x，把res乘上x
+            if n % 2 == 1:
+                res = res * x
+                n -= 1
+            # 此时累计x
+            else:
+                n /= 2
+                x = x * x
+        return res
+
+
+
 class Solution:
     """
     @param x {float}: the base number
