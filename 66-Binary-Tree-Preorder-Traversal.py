5c5d7311ad60945bbf7889ae4b17cefd5035fdef
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-03-29 23:18:36 -0400

    Create 66-Binary-Tree-Preorder-Traversal.py

---
+++ 66-Binary-Tree-Preorder-Traversal.py
@@ -0,0 +1,34 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorder_traversal(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        
+        stack = [root]
+        res = []
+
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
