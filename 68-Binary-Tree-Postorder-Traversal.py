660c0d55ea92752b348f12c8a2845e759ef89510
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-03-29 23:28:43 -0400

    Create 68-Binary-Tree-Postorder-Traversal.py

---
+++ 68-Binary-Tree-Postorder-Traversal.py
@@ -0,0 +1,32 @@
+from typing import (
+    List,
+)
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Postorder in ArrayList which contains node values.
+    """
+    def postorder_traversal(self, root: TreeNode) -> List[int]:
+        if not root: return []
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return res[::-1]
