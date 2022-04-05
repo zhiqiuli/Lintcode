d78344956cc7432401a3327fca94abacdc23df35
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-04-05 18:10:49 -0400

    Update 460-Find-K-Closest-Elements.py

--- 460-Find-K-Closest-Elements.py
+++ 460-Find-K-Closest-Elements.py
@@ -1,5 +1,5 @@
 class Solution:
-    """
+	    """
     @param A: an integer array
     @param target: An integer
     @param k: An integer
@@ -10,8 +10,9 @@ class Solution:
         # 也就是最接近 target 的两个数，他们肯定是相邻的
         right = self.findUpperClosest(A, target)
         left = right - 1
-
-    	# 两根指针从中间往两边扩展，依次找到最接近的 k 个数
+ 
+ 
+         # 两根指针从中间往两边扩展，依次找到最接近的 k 个数
         res = []
         for _ in range(k):
             if self.isLeftCloser(A, left, right, target):
@@ -20,10 +21,11 @@ class Solution:
             else:
                 res.append(A[right])
                 right += 1
-        
+       
         return res
-    
-    # 标准二分法模版
+   
+# 标准二分法模版
+# 找的是xxxxx(v)vvvvv，所以A[mid]>=target: right = mid
     def findUpperClosest(self, A, target):
         left, right = 0, len(A) - 1
         while left + 1 < right:
@@ -36,14 +38,13 @@ class Solution:
             return left
         if A[right] >= target:
             return right
-        
+       
         # 找不到的情况
         return len(A)
-    
+   
     def isLeftCloser(self, A, left, right, target):
         if left < 0:
             return False
         if right >= len(A):
             return True
-        return target - A[left] <= A[right] - target
-        
+        return target - A[left] <= A[right] – target # 相同的话，那么小的数排在前面，所以左移动
