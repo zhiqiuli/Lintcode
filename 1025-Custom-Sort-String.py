174212adbadd5575f272c820428e5fcc69209d72
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-04-23 09:51:52 -0400

    Update 1025-Custom-Sort-String.py

--- 1025-Custom-Sort-String.py
+++ 1025-Custom-Sort-String.py
@@ -5,10 +5,11 @@ class Solution:
     @return: any permutation of T (as a string) that satisfies this property
     """
     def custom_sort_string(self, S: str, T: str) -> str:
-        dict_T, res = collections.Counter(T), ''
+        dict_T, res = collections.Counter(T), '' # 使用colections.Counter来进行统计
         for char in S:
             if char in dict_T:
-                res += char * dict_T.pop(char) # char有类似list的assign方式 'a' * 5 = 'aaaaa'
+                res += char * dict_T.pop(char) # 1) 使用dict.pop()
+                                               # 2) char有类似list的assign方式 'a' * 5 = 'aaaaa'
         for char in sorted(dict_T):
             res += char * dict_T[char]
         return res
