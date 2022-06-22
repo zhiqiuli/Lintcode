4e89b263a8c774ada954dc941bea0a741a2ae844
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-06-21 20:39:09 -0400

    Update 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py

--- 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
+++ 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
@@ -8,8 +8,8 @@ class Solution:
         coolDown = 0
         noShare = 0
         for i in range(1, len(prices)):
-            withShare = max(withShare, coolDown - prices[i]) # 前一天withShare或者coolDown
-            coolDown = max(coolDown, noShare) # coolDown的位置很重要 必须在withShare和noShare之间
-            noShare = max(noShare, withShare + prices[i])
+            withShare = max(withShare, coolDown - prices[i]) # 当天withShare的话，(1)前一天同是withShare; (2)前一天coolDown并且当天买入
+            coolDown  = max(coolDown, noShare)               # 当天coolDown的话，(1)前一天同是coolDown; (2)前一天noShare。coolDown的位置很重要，必须在withShare和noShare之间
+            noShare   = max(noShare, withShare + prices[i])  # 当天noShare的话，(1)前一天同是noShare; (2)前一天withShare并且当天卖出
         return noShare
 
