8af8d62c25d1393cf75095e9a1508a7530df82de
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-06-29 13:43:23 -0400

    Update 151-Best-Time-to-Buy-and-Sell-Stock-III.py

--- 151-Best-Time-to-Buy-and-Sell-Stock-III.py
+++ 151-Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -1,18 +1,21 @@
+from typing import (
+    List,
+)
+
 class Solution:
     """
     @param prices: Given an integer array
     @return: Maximum profit
     """
-    def maxProfit(self, prices):
+    def max_profit(self, prices: List[int]) -> int:
+        # write your code here
         if not prices: return 0
-        buy1  = -prices[0] 
-        sell1 = 0
-        buy2  = -prices[0]
-        sell2 = 0
+        buy1, buy2   = -prices[0], -prices[0]
+        sell1, sell2 = 0, 0
         for i in range(1, len(prices)):
-            buy1  = max(buy1 ,       - prices[i]) # 此处存在先后顺序 只有存在buy才会有sell
-            sell1 = max(sell1, buy1  + prices[i])
-            buy2  = max(buy2 , sell1 - prices[i])
-            sell2 = max(sell2, buy2  + prices[i])
-        # return max(sell1, sell2)
+            buy1  = max(buy1  ,        - prices[i]) # 在i位置第一次buy。想要cost最小，所以总是选最小值，此处等价于min()。
+                                                    # 此处存在先后顺序，只有存在buy才会有sell。
+            sell1 = max(sell1 ,  buy1  + prices[i]) # 在i位置第一次sell。想要profit最大。
+            buy2  = max(buy2  ,  sell1 - prices[i]) 
+            sell2 = max(sell2 ,  buy2  + prices[i])
         return sell2 # 只需要return sell2即可，sell2包含了sell1的profit
