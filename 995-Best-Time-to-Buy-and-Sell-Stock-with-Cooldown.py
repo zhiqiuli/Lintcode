cc43d19e5e0060a9784be22e988771b60f216ef0
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-06-29 16:05:32 -0400

    Update 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py

--- 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
+++ 995-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
@@ -13,3 +13,22 @@ class Solution:
             noShare   = max(noShare, withShare + prices[i])  # 当天noShare的话，(1)前一天同是noShare; (2)前一天withShare并且当天卖出
         return noShare
 
+from typing import (
+    List,
+)
+
+# 内容和前一版本一样，只是变量名不太一样
+class Solution:
+    """
+    @param prices: a list of integers
+    @return: return a integer
+    """
+    def max_profit(self, prices: List[int]) -> int:
+        buy  = -prices[0]
+        cool = 0
+        sell = 0
+        for i in range(1, len(prices)):
+            buy  = max(buy , cool - prices[i]) # 如果买入 前一天肯定是cool
+            cool = max(cool, sell)
+            sell = max(sell, buy  + prices[i])
+        return sell
