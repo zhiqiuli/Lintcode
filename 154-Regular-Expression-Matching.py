e182e08ed65c698efdcdd9b953dd0bb189400768
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2022-08-26 21:10:02 -0400

    Update 154-Regular-Expression-Matching.py

--- 154-Regular-Expression-Matching.py
+++ 154-Regular-Expression-Matching.py
@@ -16,17 +16,22 @@ class Solution:
 
         if p[j:] == '':
             return s[i:] == ''
-
+        
+        # s='aab' & p='a*b'
         if j + 1 < len(p) and p[j + 1] == '*':
+                      # s='aab' & p='b'
             matched = self.dfs(s, p, i, j + 2, memo) or \
+                      # good, then --------------------> s='ab' & p='a*b'
                       ((s[i] == p[j] or p[j] == '.') and self.dfs(s, p, i + 1, j, memo))
-        else:
+        # s='aab' & p='abc'
+        else:          # good, then -------------------> s='ab' & p='bc'
             matched =  (s[i] == p[j] or p[j] == '.') and self.dfs(s, p, i + 1, j + 1, memo)
         
         memo[(i, j)] = matched
         return memo[(i, j)]
     
-    # if s='' and p='a*b*' this will return True as well
+    # pattern = 'a*b*' returns True
+    # pattern = 'abc*' return False
     def is_empty(self, pattern):
         if len(pattern) % 2 == 1:
             return False
