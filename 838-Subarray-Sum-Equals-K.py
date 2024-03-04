194c33a78b260ac053fdef80d5027642fb7708e6
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2024-03-03 22:20:17 -0500

    Update 838-Subarray-Sum-Equals-K.py

--- 838-Subarray-Sum-Equals-K.py
+++ 838-Subarray-Sum-Equals-K.py
@@ -1,21 +1,45 @@
-from typing import (
-    List,
-)
-
+'''
+计算Prefix_sum的过程中，
+用HashMap 记录当前prefix_sum出现的次数，
+当prefix_sum - k 出现在HashMap中，叠加count
+时间复杂度为O(n)
+'''
 class Solution:
-    """
-    @param nums: a list of integer
-    @param k: an integer
-    @return: return an integer, denote the number of continuous subarrays whose sum equals to k
-    """
-    def subarray_sum_equals_k(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        sum_ = 0
-        # 假设 nums=[1] & k=1
-        presum_count = {0:1}
-        res = 0
-        for i in range(len(nums)):
-            sum_ += nums[i]
-            res += presum_count.get(sum_ - k, 0)
-            presum_count[sum_] = presum_count.get(sum_, 0) + 1
-        return res
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        count = 0 
+        prefix_sum = 0
+        prefix_sum_to_times = {0: 1}
+        
+        for num in nums:
+            
+            prefix_sum += num 
+
+            '''
+            consider k = 3
+            6
+            [....][.........]
+            6
+            [..........][...]
+            6
+            [............][.]
+            so prefix_sum_to_times[6] = 3
+            9
+            [...............]
+            therefore prefix_sum_to_times[9 - 3] = 3
+            count += prefix_sum_to_times[9 - 3]
+            '''
+            
+            if prefix_sum - k in prefix_sum_to_times:
+                
+                count += prefix_sum_to_times[prefix_sum - k]
+                
+            if prefix_sum in prefix_sum_to_times:
+                
+                prefix_sum_to_times[prefix_sum] += 1 
+                
+            else:
+                
+                prefix_sum_to_times[prefix_sum] = 1 
+            
+        return count
