125958f5448f8fd9e8c0fb43d9c1c0c53ea01069
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2024-03-08 00:08:56 -0500

    Update 402-Continuous-Subarray-Sum.py

--- 402-Continuous-Subarray-Sum.py
+++ 402-Continuous-Subarray-Sum.py
@@ -2,24 +2,35 @@ from typing import (
     List,
 )
 
+import sys
+
 class Solution:
     """
     @param a: An integer array
     @return: A list of integers includes the index of the first number and the index of the last number
+
+    核心思路还是同向双指针，枚举右端点，左端点不回头，
+    时间复杂度O(n)
     """
     def continuous_subarray_sum(self, a: List[int]) -> List[int]:
-        runningSum = maxSum = a[0]
-        maxLo = maxHi = lo = hi = 0
-        # enumerate 可以选择从某一个index开始，譬如说这里就是start=1
-        for hi, num in enumerate(a[1:], start=1):
-            # 如果running < 0，reset runningSum
-            if runningSum < 0:
-                runningSum = num
-                lo = hi
-            else:
-                runningSum += num
+        left = 0
+        presum = 0
+        res = [-1, -1]
+
+        max_sum = -sys.maxsize
+
+        for right in range(len(a)):
             
-            if runningSum > maxSum:
-                maxLo, maxHi, maxSum = lo, hi, runningSum
-        
-        return maxLo, maxHi
+            # if presum < 0, then reset presum as a[right] & update left
+            # otherwise update presum
+            if presum < 0:
+                presum = a[right]
+                left = right
+            else:
+                presum += a[right]
+            
+            if presum > max_sum:
+                max_sum = presum
+                res[0], res[1] = left, right
+
+        return res
