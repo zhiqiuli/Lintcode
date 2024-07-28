cc36baacfa278d0ec97be2df7033797a842824ea
Zhiqiu Li <zhiqiuli.edu@gmail.com>
2024-07-27 21:54:49 -0400

    Update 15-Permutations.py

--- 15-Permutations.py
+++ 15-Permutations.py
@@ -28,3 +28,24 @@ class Solution:
             self.dfs(nums, perm, res, visited)
             perm.pop()
             visited[i] = False
+
+# another solution with using visited
+class Solution:
+    """
+    @param nums: A list of integers.
+    @return: A list of permutations.
+             we will sort your return value in output
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    def dfs(self, nums, path, res):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+        for n in nums:
+            if n not in path:
+                path.append(n)
+                self.dfs(nums, path, res)
+                path.pop()
